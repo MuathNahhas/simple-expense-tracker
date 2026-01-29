0715181898f81b9e9e06900cb9cba3701dd0a617
Muath <muath.nahhas@yahoo.com>
2026-01-29 18:29:40 +0300

    remove auth from category

--- qashio-api/src/categories/controller/categories.controller.ts
+++ qashio-api/src/categories/controller/categories.controller.ts
@@ -8,9 +8,8 @@ import {
 } from '@nestjs/common';
 import { CategoriesService } from '../service/categories.service';
 import { CreateCategoryDto } from '../dto/create-category.dto';
-import { Authentication } from '../../authentication/decorators/authentication.decorator';
 
-@Authentication()
+
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
